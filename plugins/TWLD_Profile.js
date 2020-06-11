f878706641b8f37c334ad4782136b76ca9b7e67c
kapuusagi <usagizinrui@yahoo.co.jp>
2020-06-12 08:17:29 +0900

    TWLD_Profileプラグインが動作したので追加しておく。

---
+++ plugins/TWLD_Profile.js
@@ -0,0 +1,59 @@
+/*:
+ * @plugindesc プロフィールシステム
+ */
+
+var Imported = Imported || {};
+Imported.TWLD_Profile = true;
+
+
+var TWLD = TWLD || {};
+TWLD.Profile = TWLD.Profile || {};
+
+// for ESLint
+if (typeof DataManager === 'undefined') {
+    var DataManager = {};
+    var Game_Actor = {};
+}
+
+var $dataProfiles = null;
+
+(function() {
+    TWLD.Profile.DataManager_loadDatabase = DataManager.loadDatabase;
+    DataManager.loadDatabase = function() {
+        TWLD.Profile.DataManager_loadDatabase.call(this);
+        try {
+            this.loadDataFile("$dataProfiles","ActorProfiles.json");
+        }
+        catch (e) {
+            console.error(e);
+            $dataProfiles = null;
+        }
+    };
+
+    //------------------------------------------------------------------------------
+    // Game_Actorの変更
+    // 
+
+    TWLD.Profile.Game_Actor_profile = Game_Actor.prototype.profile;
+
+    /**
+     * プロフィールテキストを取得する。
+     * @return {String} プロフィール文字列
+     */
+    Game_Actor.prototype.profile = function() {
+        if ($dataProfiles) {
+            var actorProfile = $dataProfiles[this._actorId];
+            if (actorProfile) {
+                for (var i = 0; i < actorProfile.profiles.length; i++) {
+                    var prof = actorProfile.profiles[i];
+                    if (!prof.condition || eval(prof.condition)) {
+                        return prof.text;
+                    }
+                }
+            }
+        }
+        // 該当するものが無い場合にはこれまで通りのプロフィールデータを返す。    
+        return TWLD.Profile.Game_Actor_profile.call(this);
+    };
+
+})();
